8515329ff3cd32aa389def28b6a6dfa11c3d8b5c
agunapala <agunapala@wiley.com>
2022-04-29 13:46:23 +0530

    First test case add a new task to todo list is completed

---
+++ cypress/integration/todo_tests/todo.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+beforeEach(() => {
+  const newItem = 'Go through cypress tutorial'
+
+  cy.visit('http://localhost:3000/')
+
+
+  cy.get('input', { setTimeout: 5000 }).type(`${newItem}{enter}`)
+})
+
+it('add a new task to todo list', () => {
+
+  cy.get('span')
+  cy.should('have.text', 'Go through cypress tutorial')
+
+  cy.get('.App-tick').should('not.be.checked')
+})
